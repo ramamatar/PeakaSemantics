fac3ad0d51347c6a06d7b9dc337748b15f7f3a90
ramamatar <166447147+ramamatar@users.noreply.github.com>
2025-07-22 15:39:29 +0300

    Update test_peaka.py

--- test_peaka.py
+++ test_peaka.py
@@ -26,7 +26,7 @@ project_id = project_info.get("projectId")
 
 
 results=[]
-target_databases = {"AIRLINES","dELIVERY_CENTER","MUSIC"}
+target_databases = {"AIRLINES","DELIVERY_CENTER","MUSIC"}
 for item in data:
     database = item.get("database")
     question = item.get("instruction")
